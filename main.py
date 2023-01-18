d61950b4ff05dfbb67b5f19aa057ca6089529162
u-kai <76635578+u-kai@users.noreply.github.com>
2023-01-18 11:09:15 +0000

    add extensions and index.html

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
 import uvicorn
 from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
 
 from lib.hello import hello
 
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],      
+    allow_headers=["*"]       
+)
 @app.get("/")
 async def hello_api():
     return {"message":hello()}
